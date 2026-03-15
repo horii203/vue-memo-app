939f16c50b15d5a5bc11897c0e3fc99b4424a99d
horii203 <89522294+horii203@users.noreply.github.com>
2026-03-15 16:48:09 +0900

    入力フィールドとループ表示の実装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+// とりあえず名前と趣味だけ
+const name = ref("");
+const hobby = ref("");
+
+const people = ref<{ name: string; hobby: string }[]>([]);
+
+const addPerson = () => {
+  if (!name.value || !hobby.value) return;
+
+  people.value.push({
+    name: name.value,
+    hobby: hobby.value,
+  });
+
+  name.value = "";
+  hobby.value = "";
+};
 </script>
 
 <template>
-  <HelloWorld />
+  <h1>メモアプリ</h1>
+
+  <!-- 入力フィールド -->
+  <div>
+    <input v-model="name" placeholder="名前" />
+    <input v-model="hobby" placeholder="趣味" />
+    <button @click="addPerson">追加</button>
+  </div>
+
+  <ul>
+    <!-- 配列をループ表示 -->
+    <li v-for="person in people" :key="person.name">
+      {{ person.name }}
+    </li>
+  </ul>
 </template>
